87f428687d635cd0a8489d373928a2bd917a0555
Vivian Lo <yunyubee@gmail.com>
2019-10-07 14:54:57 +0800

    Add mongoose connection and create url model

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-
 const port = 3000
+
+//connect mongoose database
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/url', { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// include mongoose models
+const Url = require('./models/url')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
+// routers
 app.get('/', (req, res) => {
   res.send('hello')
 })
