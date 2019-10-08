2d191414eb7299e64346df3518c53cb0c92849db
Vivian Lo <yunyubee@gmail.com>
2019-10-08 15:23:18 +0800

    Add routers of create short url and connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const port = 3000
 
 //connect mongoose database
@@ -20,11 +21,44 @@ const Url = require('./models/url')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 // routers
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+
+app.post('/', (req, res) => {
+  const longUrl = req.body.longUrl
+  const newUrl = new Url({
+    longUrl: longUrl,
+    shortUrl: ""
+  })
+  const shortUrl = newUrl._id.toString().slice(-5)
+  newUrl.shortUrl = shortUrl
+
+  newUrl.save((err) => {
+    if (err) return console.log(err)
+
+    console.log('the new URL is added')
+    //flash: 短網址產生結果
+    res.redirect('/')
+  })
+})
+
+app.get('/:shortUrl', (req, res) => {
+  Url.findOne({ shortUrl: req.params.shortUrl }, (err, url) => {
+    if (err) throw err
+
+    if (url) {
+      res.redirect(url.longUrl)
+    } else {
+      //flash: 短網誌有誤
+      res.redirect('/')
+    }
+  })
 })
 
 app.listen(port, () => {
--- models/url.js
+++ models/url.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const urlSchema = new Schema({
-  url: {
+  longUrl: {
     type: String,
     required: true
   },
-  shortenerUrl: {
+  shortUrl: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }
 })
 
